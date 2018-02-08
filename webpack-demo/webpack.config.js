0867e395eca334abb377401efed53290e62498b7
VIGDXX <VIGDXX@gmail.com>
2018-02-08 20:38:43 +0800

    output update

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,8 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app1: './src/index.js',
+        print2: './src/print.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+        title: 'Output Management'
+        })
+    ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     }
 };
