a460faab9604e0451c32bc94a2212d36c10e2583
VIGDXX <VIGDXX@gmail.com>
2018-02-12 15:06:00 +0800

    'ajax'

--- js/README.md
+++ js/README.md
@@ -2,3 +2,22 @@
 
 # hash and history
 	hash mode 
+# ajax
+```
+function ajax(option) {
+	var xhr = XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP")
+	// open 之前做监听
+	xhr.onreadystatechange = function() {
+		if(xhr.readydtate = 4) {
+			return xhr.response
+		}
+	}
+	xhr.upload.onprogress = function(e) {
+		console.log(e)
+	}
+	xhr.open(option.method,option.url,true);
+	// open 之后 setRequestHeader
+	xhr.setRequestHeader("my-header","test");
+	xhr.send(option.formdata || null);
+}
+```
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,19 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
 module.exports = {
     entry: {
-        app1: './src/index.js',
-        print2: './src/print.js'
+        app: './src/index.js',
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
         title: 'Output Management'
-        })
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
     ],
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase:'./dist',
+        hot: true
+    },
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     }
 };
