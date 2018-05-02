43b064b98a71e011fb119d793238d9586ab48f1f
vigdxx <vigdxx@gmail.com>
2018-05-02 23:09:43 +0800

    .

--- nodejs/project/server.js
+++ nodejs/project/server.js
@@ -1,11 +1,13 @@
 const http = require('http') 
+const url = require('url')
 const util = require('./util') 
 
 const server = http.createServer( (request, response) => {
+    console.log(request)
     var cookies = util.parseCookie(request.headers.cookie)
-    console.log(cookies.sessionid)
+    console.log(cookies)
     response.setHeader('Content-Type', 'text/html');
     response.setHeader('Set-Cookie', 'sessionid=38afes7a8; HttpOnly; Path=/');
-    response.end('cnm')
+    response.end('hello')
 })
 server.listen(8080)
